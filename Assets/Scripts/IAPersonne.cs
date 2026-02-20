5e56d123f84cf1572d6ef22c6047aa85498352c2
tag5T1 <146302657+tag5T1@users.noreply.github.com>
2026-02-20 17:49:43 -0500

    Fin prototype
    Tweak d'infection et pathfinding améliorer.

--- Assets/Scripts/IAPersonne.cs
+++ Assets/Scripts/IAPersonne.cs
@@ -8,12 +8,13 @@ public class IAPersonne : MonoBehaviour
     [SerializeField] GameObject particuleDeBase;
     NavMeshAgent agent;
     public Personne personne { get; private set; }
+    string location;
+
 
 
     public void Creation()
     {
         personne = new Personne();
-        personne.virus = new Virus(transform, Random.Range(50, 80) / 10, 10, Random.Range(55, 85), 0.5f, 0.1f);
     }
     private void Start()
     {
@@ -23,27 +24,43 @@ public class IAPersonne : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-            personne.niveauToux = 999;
-
-        if (personne.niveauToux - personne.virus.niveauMin > Random.Range(20, 50))
+        if (personne.infecté)
         {
-            Toux();
-            personne.niveauToux = 0;
-        }
-        else
-            personne.niveauToux += 25 * Time.deltaTime;
+            // LOGIQUE MUST BE POPPED
+            if (personne.niveauToux - personne.virus.niveauMin > Random.Range(20, 50))
+            {
+                Toux();
+                personne.niveauToux = 0;
+            }
+            else
+                personne.niveauToux += 25 * Time.deltaTime;
 
             Debug.DrawRay(transform.position, transform.forward * 5, Color.green, 0.1f);
+        }
+        
         transform.LookAt(transform.position + agent.velocity);
     }
 
 
     private IEnumerator Pathfinding()
     {
-        Vector3 destination = personne.GénérerPosition();
-        agent.SetDestination(destination);
-        yield return new WaitForSeconds(Random.Range(3, 10));
+        Vector3 destination;
+        if (location == "Bureau")
+        {
+            destination = new Vector3(Random.Range(-20, 20), -5, Random.Range(-20, 20));
+            agent.SetDestination(destination);
+            yield return new WaitUntil(() => new Vector2(transform.position.x, transform.position.z) == new Vector2(destination.x, destination.z));
+            location = "Roam";
+        }
+        else
+        {
+            destination = personne.GénérerPosition();
+            agent.SetDestination(destination);
+            yield return new WaitUntil(() => new Vector2(transform.position.x, transform.position.z) == new Vector2(destination.x, destination.z));
+            location = "Bureau";
+        }
+
+        yield return new WaitForSeconds(Random.Range(10, 30)/10);
         StartCoroutine(Pathfinding());
     }
 
@@ -61,7 +78,7 @@ public class IAPersonne : MonoBehaviour
             instance = GameObject.Instantiate(prefab, pos, transform.rotation);
             VirusParticule vir = instance.GetComponent<VirusParticule>();
             Debug.DrawRay(transform.position, transform.forward * 5, Color.blue, 2f);
-            vir.Creation(transform.forward, personne.virus);
+            vir.Création(this.gameObject, transform.forward, personne.virus);
         }
     }
 }
--- Assets/Scripts/VirusParticule.cs
+++ Assets/Scripts/VirusParticule.cs
@@ -5,23 +5,23 @@ public class VirusParticule : MonoBehaviour
     GameObject personneÉmettrice;
     Rigidbody rb;
     Virus virus;
-    float maxSpread = 20;
     float force; // Force à laquelle la particule est projetée
     float gravité; // Force appliquée vers le bas
     float duréeVie; // Tremps de vie avant de mourir
     float tempsVie; // Temps de vie depuis sa création
 
-    public void Creation(GameObject personne, Vector3 directionEmission, Virus virus)
+    public void Création(GameObject personne, Vector3 directionEmission, Virus virus)
     {
         personneÉmettrice = personne;
         rb = GetComponent<Rigidbody>();
         this.virus = virus;
         force = virus.force;
         gravité = virus.gravité;
+        rb.linearDamping = Random.Range(virus.decceleration*500, virus.decceleration*1500)/1000;
         duréeVie = virus.duréeVie;
         tempsVie = 0;
 
-        var forceVectorielle = directionEmission * Random.Range(0, force) + new Vector3((float)Random.Range(-maxSpread, maxSpread) / 10, (float)Random.Range(-maxSpread, maxSpread) / 10, (float)Random.Range(-maxSpread, maxSpread) / 10);
+        var forceVectorielle = directionEmission * Random.Range(0, force) + new Vector3((float)Random.Range(-virus.maxSpread, virus.maxSpread) / 10, (float)Random.Range(-virus.maxSpread, virus.maxSpread) / 10, (float)Random.Range(-virus.maxSpread, virus.maxSpread) / 10);
         rb.AddForce(forceVectorielle, ForceMode.Impulse);
         transform.LookAt(transform.position + forceVectorielle);
         Debug.DrawRay(transform.position, forceVectorielle, Color.red, 1);
@@ -41,7 +41,7 @@ public class VirusParticule : MonoBehaviour
         if (collision.gameObject.tag == "Personne" && collision.gameObject != personneÉmettrice)
         {
             Debug.Log(personneÉmettrice.name + " --> " + collision.gameObject.name);
-            collision.gameObject.GetComponent<Personne>().Infecter(virus);
+            collision.gameObject.GetComponent<IAPersonne>().personne.Infecter(virus);
         }
     }
 }
